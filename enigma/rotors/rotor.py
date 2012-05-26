208efd5f9ec0a36d8d55a1319c8f8ad3174b4d47
Brian Neal <bgneal@gmail.com>
2012-05-26 12:53:00 -0500

    Futzing around with the doc string & copyright comment order.

--- enigma/rotors/rotor.py
+++ enigma/rotors/rotor.py
@@ -1,10 +1,9 @@
-"""rotor.py - this module contains the Rotor class for the Enigma simulation.
+# Copyright (C) 2012 by Brian Neal.
+# This file is part of Py-Enigma, the Enigma Machine simulation.
+# Py-Enigma is released under the MIT License (see License.txt).
 
-Copyright (C) 2012 by Brian Neal.
-This file is part of Py-Enigma, the Enigma Machine simulation.
-Py-Enigma is released under the MIT License (see License.txt).
+"""rotor.py - this module contains the Rotor class for the Enigma simulation."""
 
-"""
 import string
 import collections
 
--- enigma/tests/test_rotor.py
+++ enigma/tests/test_rotor.py
@@ -1,7 +1,8 @@
-"""test_rotor.py - Unit tests for the Rotor class for the Enigma simulation."""
 # Copyright (C) 2012 by Brian Neal.
 # This file is part of Py-Enigma, the Enigma Machine simulation.
 # Py-Enigma is released under the MIT License (see License.txt).
+
+"""test_rotor.py - Unit tests for the Rotor class for the Enigma simulation."""
 
 import unittest
 import collections
