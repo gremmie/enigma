cd96d34219231d2dcbcd14b2f3fe93fc9ab9f189
Brian Neal <bgneal@gmail.com>
2012-05-30 19:16:22 -0500

    Created a setup.py using distutils.

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Copyright (C) 2012 by Brian Neal.
+# This file is part of Py-Enigma, the Enigma Machine simulation.
+# Py-Enigma is released under the MIT License (see License.txt).
+
+from distutils.core import setup
+from os.path import join, dirname
+
+import enigma
+
+setup(
+    name='enigma',
+    version=enigma.__version__,
+    author='Brian Neal',
+    author_email='bgneal@gmail.com',
+    url='https://bitbucket.org/bgneal/enigma/',
+    license='MIT',
+    description='A historically accurate Enigma machine simulation library.',
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    packages=['enigma', 'enigma.rotors', 'enigma.tests'],
+    package_data=dict(enigma=['examples/*.py']),
+    scripts=['pyenigma.py'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications',
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
